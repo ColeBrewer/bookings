4c989da70b38ea4119fa66f10a364a1d7b8305e2
ColeBrewer <cole.brewerton@gmail.com>
2023-08-18 15:09:04 +1200

    adding email

--- script.js
+++ script.js
@@ -27,38 +27,43 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
 }
 
-function sendEmail() {
 
-  Email.send({  /* Emails currently sending from my school email to my home email to prove functioning,
-                   If stakeholder chooses to use this I will put in his email information and make a new email specifcally for sending bookings*/
-     
-    Host: "smtp.gmail.com",
-    Username: "9cbrewer@hbhs.school.nz",
-    Password: "hyper3cum",
-    To: 'cole.brewerton@gmail.com', 
-    From: "9cbrewer@hbhs.school.nz",
-    Subject: "New Facility Booking",
-    Body: document.getElementById(fname) +
-          document.getElementById(lname) +
-          document.getElementById(email) +
-          document.getElementById(number) +
-          document.getElementById(facility) +
-          document.getElementById(stime) +
-          document.getElementById(etime) +
-          document.getElementById(comments),
+function sendingEmail(ele) {
 
-  })
+
+  let emailContents = {
+
+    fromMessage:
+      "First Name: " + document.getElementById("fname").value + '\n' +
+      "Last Name: " + document.getElementById("lname").value + '\n' +
+      "Email: " + document.getElementById("email").value + '\n' +
+      "Phone Number: " + document.getElementById("number").value + '\n' +
+      "Facility: " + document.getElementById("facility").value + '\n' +
+      "Starting Time: " + document.getElementById('stime').value + '\n' +
+      "Ending Time: " + document.getElementById("etime").value + '\n' +
+      "Comments: " + document.getElementById("comments").value,
+
+    email: document.getElementById("email").value
+
+
+  }
+
+
+
+
+  emailjs.send('service_4rp61kq', 'template_7mh46q8', emailContents)
     .then(function () {
-      alert("Email Sent") 
-    });
+      alert("Booking Submitted - Under Review")
+    })
+
+
+
+
+
+
+
+
 }
-
-
-
-
-
-
-
 
 
 
